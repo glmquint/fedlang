34be52fc4d572d34947ced59b50879e65421b36e
Fabio <fabizen99@gmail.com>
2024-08-20 19:03:10 +0200

    first idea for the process_client feature

--- src/go/client/fcmeans_client.go
+++ src/go/client/fcmeans_client.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"bytes"
 	"fcmeans/common"
+	"github.com/MacIt/pickle"
+	"github.com/ergo-services/ergo"
+	"github.com/ergo-services/ergo/etf"
+	"github.com/ergo-services/ergo/gen"
+	"github.com/ergo-services/ergo/node"
+	"gonum.org/v1/gonum/mat"
+	"log"
+	"os"
 )
+
 type FCMeansClient struct {
 	*common.FedLangProcess
-
 }
 type FLExperiment struct {
 	_global_model_parameters    [][]float64
@@ -29,11 +38,58 @@ type FLExperiment struct {
 }
 
 func (f *FCMeansClient) init_client() {
-	f.FedLangProcess = common.NewFedLangProcess()
+	f.FedLangProcess = common.FedLangProcess()
 }
-func (f *FCMeansClient) process_client() {
+func (f *FCMeansClient) process_client(expertiment FLExperiment, round_number int, centers_param []byte) {
+	//data := nil
+	log.Printf("start process_client, expertiment = %v, round_number = %v, centers_param = %v\n", expertiment, round_number, centers_param)
+	var centers_list [][]float64
+	decoder := pickle.NewDecoder(bytes.NewReader(centers_param))
+	decodedResult_tmp, err := decoder.Decode()
+	if err != nil {
+		log.Println("Error decoding:", err)
+		panic(err)
+	}
+	for _, v := range decodedResult_tmp.([]interface{}) {
+		arr := make([]float64, 0)
+		for _, vv := range v.([]interface{}) {
+			arr = append(arr, vv.(float64))
+		}
+		centers_list = append(centers_list, arr)
+	}
+	log.Printf("centers_list = %v\n", centers_list)
+
+	centers := mat.NewDense(len(centers_list), len(centers_list[0]), nil)
+	for i := 0; i < len(centers_list); i++ {
+		centers.SetRow(i, centers_list[i])
+	}
+	//array of size 10
+	dataFrame_STUB := make([][]float64, 10)
+	factorLambda_STUB := 0.1
+	// TODO: this is a stub, need to implement the actual logic and types
+	numClusters := len(centers.RawMatrix().Data)
+	numObjects := len(dataFrame_STUB) // Assuming X is defined elsewhere
+	factorLambda := factorLambda_STUB // Assuming factorLambda is defined elsewhere
+	numFeatures := centers.RawMatrix().Cols
+
+	// Initialize ws and u slices
+	ws := mat.NewDense(numClusters, numFeatures, nil)
+	u := mat.NewVecDense(numClusters, nil)
+
+	for i := 0; i < numFeatures; i++ {
+		denom := 0.0
+		numer := [0]*numClusters
+		x = dataFrame_STUB[i]
+		// TODO: continue with the translation
+		//for j := 0; j < numClusters; j++ {
+
+		//}
+	}
+
 }
 func (f *FCMeansClient) destroy() {
+	//f.FedLangProcess.Terminate(f.FedLangProcess.Process, "stop")
+	log.Println("FCMeansClient terminated")
 }
 
 func main() {
