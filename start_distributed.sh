3804946543fa5bb7b6b98109a7485686eb693b13
giacomovolpi <giacomo.volpi99@gmail.com>
2024-08-22 15:38:44 +0200

    Fix docker-compose

--- start_distributed.sh
+++ start_distributed.sh
@@ -3,7 +3,7 @@
 # Function to clean up tmux session and Docker containers
 cleanup() {
   tmux kill-session -t fedlang_containers
-  docker-compose down
+  docker compose down
 }
 
 # Trap the EXIT signal to ensure cleanup is run when the script exits
@@ -13,21 +13,21 @@ trap cleanup EXIT
 tmux new-session -d -s fedlang_containers
 
 # Run the director container in the first pane
-tmux send-keys -t fedlang_containers "docker-compose up director; sleep 1" C-m
+tmux send-keys -t fedlang_containers "docker compose up director; sleep 1" C-m
 
 # Split the window horizontally and run the client0 container
 tmux split-window -h
-tmux send-keys "docker-compose up client0" C-m
+tmux send-keys "docker compose up client0" C-m
 
 # Split the first pane vertically and run the client1 container
 tmux select-pane -t 0
 tmux split-window -v
-tmux send-keys "docker-compose up client1; sleep 1" C-m
+tmux send-keys "docker compose up client1; sleep 1" C-m
 
 # Split the original pane vertically and run the server container
 tmux select-pane -t 2
 tmux split-window -v
-tmux send-keys "docker-compose up server" C-m
+tmux send-keys "docker compose up server" C-m
 
 # Adjust the layout to tiled to fit all panes
 tmux select-layout tiled
