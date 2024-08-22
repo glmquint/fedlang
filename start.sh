6e6c46a27b932f0b01367bb9ac5292330a72ff51
Guillaume Quint <glmquint@gmail.com>
2024-08-22 16:05:51 +0200

    longer sleep before stat node

--- start.sh
+++ start.sh
@@ -10,7 +10,7 @@ tmux split-window -h "pipenv run sh run_clients.sh 0 0; read"
 tmux select-pane -t 0
 tmux split-window -v "pipenv run sh run_clients.sh 1 1; read"
 # Split the original pane vertically and run the Erlang node
-sleep 1
+sleep 2
 tmux select-pane -t 2
 tmux split-window "erlc stats_node.erl && erl -name stats_node_test@127.0.0.1 -setcookie 'cookie_123456789' -kernel prevent_overlapping_partitions false -noshell -s stats_node test$1 -s init stop; read"; 
 # Attach to the tmux session
