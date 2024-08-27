2253dcda5048dc722a01831ab7710e31f7206bfd
Guillaume Quint <glmquint@gmail.com>
2024-08-27 18:19:55 +0200

    make deterministic experiments

--- src/go/client/fcmeans_client.go
+++ src/go/client/fcmeans_client.go
@@ -138,7 +138,8 @@ func load_experiment_info(numClients int, targetFeature int, datasetName ...stri
 		dfX = append(dfX, row)
 	}
 
-	randomSamples := rand.Perm(len(dfX))[:size]
+	random_gen := rand.New(rand.NewSource(42))
+	randomSamples := random_gen.Perm(len(dfX))[:size]
 	var values [][]float64
 	var yTrueSample []float64
 	for _, idx := range randomSamples {
